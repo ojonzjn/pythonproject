b94a83bfb3312e7a6d762dcfa776a29201e8f608
ojonzjn <ssenpick@naver.com>
2022-05-23 15:01:20 +0900

    2022/05/23/14:51 오라클 라이브러리 설치내용 추가

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -10,16 +10,16 @@ for item in cursor:
 
 conn.close()
 
-# import cx_Oracle
+import cx_Oracle
 
-# conn = cx_Oracle.connect('scott','tiger','192.168.1.11:1521/XE')
-# cursor = conn.cursor()
-# k = input("job을 입력하세요 : ")
-# cursor.execute(f"select * from emp where job = '{k}'")
-# for item in cursor:
-#     print(item[1:8])
+conn = cx_Oracle.connect('scott','tiger','192.168.1.11:1521/XE')
+cursor = conn.cursor()
+k = input("job을 입력하세요 : ")
+cursor.execute(f"select * from emp where job = '{k}'")
+for item in cursor:
+    print(item[1:8])
 
-# m = input("사원의 이름 일부를 입력하세요 : ")
-# cursor.execute(f"select * from emp where ename like '%{m}%'")
-# for item in cursor:
-#     print(item[1:8])
+m = input("사원의 이름 일부를 입력하세요 : ")
+cursor.execute(f"select * from emp where ename like '%{m}%'")
+for item in cursor:
+    print(item[1:8])
