bd164cc2049e9cc0b6c8768122cb7f6a38eb8f23
ojonzjn <ssenpick@naver.com>
2022-05-23 14:51:39 +0900

    2022/05/23/14:51 오라클 라이브러리 설치내용 추가

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -1,25 +1,25 @@
-# import cx_Oracle
-
-# conn = cx_Oracle.connect('scott','tiger','192.168.1.11:1521/XE')
-# cursor = conn.cursor()
-# cursor.execute("select * from emp")
-
-# print(cursor)
-# for item in cursor:
-#     print(item[1],item[5])
-
-# conn.close()
-
 import cx_Oracle
 
 conn = cx_Oracle.connect('scott','tiger','192.168.1.11:1521/XE')
 cursor = conn.cursor()
-k = input("job을 입력하세요 : ")
-cursor.execute(f"select * from emp where job = '{k}'")
-for item in cursor:
-    print(item[1:8])
+cursor.execute("select * from emp")
 
-m = input("사원의 이름 일부를 입력하세요 : ")
-cursor.execute(f"select * from emp where ename like '%{m}%'")
+print(cursor)
 for item in cursor:
-    print(item[1:8])
+    print(item[1],item[5])
+
+conn.close()
+
+# import cx_Oracle
+
+# conn = cx_Oracle.connect('scott','tiger','192.168.1.11:1521/XE')
+# cursor = conn.cursor()
+# k = input("job을 입력하세요 : ")
+# cursor.execute(f"select * from emp where job = '{k}'")
+# for item in cursor:
+#     print(item[1:8])
+
+# m = input("사원의 이름 일부를 입력하세요 : ")
+# cursor.execute(f"select * from emp where ename like '%{m}%'")
+# for item in cursor:
+#     print(item[1:8])
