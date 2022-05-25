e84aa97a5c076d15083502f16fff246ffcfe32f4
ojonzjn <ssenpick@naver.com>
2022-05-25 17:18:57 +0900

    2022/5/25 중간커밋

--- day07_dbtest/create_fruit.py
+++ day07_dbtest/create_fruit.py
@@ -2,4 +2,5 @@ import cx_Oracle
 
 conn = cx_Oracle.connect('scott','tiger','192.168.1.11:1521/XE')
 cursor = conn.cursor()
-cursor.execute("create table FRUIT(FNAME VARCHAR2(20),PRICE NUMBER(10),COST NUMBER(10),COUNT NUMBER(10))")
+cursor.execute("drop table fruit")
+cursor.execute("create table FRUIT(FNAME VARCHAR2(20) primary key,PRICE NUMBER(10),COST NUMBER(10),COUNT NUMBER(10))")
